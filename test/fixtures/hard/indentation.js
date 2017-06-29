a685b375e146afb84d33d5c76c6690f351fa0156
Thomas Ingram <ravinggenius@users.noreply.github.com>
2017-06-29 16:56:14 -0400

    fixup f5fa473b

--- test/fixtures/hard/indentation.js
+++ test/fixtures/hard/indentation.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 
 // None of the below should throw indentation errors
 const Comp = () => {
