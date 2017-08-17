15093e0594b007941d29d17751bc8de40e65a346
ismay <hello+github@ismaywolff.nl>
2017-08-17 12:10:44 +0200

    Remove ignoring of stylelint rules

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 const path = require('path')
 const parse = require('./parsers/index')
 // TODO Fix ampersand in selectors
-// TODO ENFORCE THESE RULES
-// value-no-vendor-prefix – don't allow vendor prefixes
-// property-no-vendor-prefix – don't allow vendor prefixes
-
-const ignoredRules = [
-  // Don't throw if there's no styled-components in a file
-  'no-empty-source',
-  // We don't care about end-of-source newlines, users cannot control them
-  'no-missing-end-of-source-newline'
-]
 
 const sourceMapsCorrections = {}
 
@@ -30,23 +20,15 @@ module.exports = (/* options */) => ({
   // Fix sourcemaps
   result(stylelintResult, filepath) {
     const lineCorrection = sourceMapsCorrections[filepath]
-    const newWarnings = stylelintResult.warnings.reduce((prevWarnings, warning) => {
-      if (ignoredRules.includes(warning.rule)) return prevWarnings
-      const correctedWarning = Object.assign(warning, {
+    const warnings = stylelintResult.warnings.map(warning =>
+      Object.assign(warning, {
         // Replace "brace" with "backtick" in warnings, e.g.
         // "Unexpected empty line before closing backtick" (instead of "brace")
         text: warning.text.replace(/brace/, 'backtick'),
         line: lineCorrection[warning.line]
       })
-      prevWarnings.push(correctedWarning)
-      return prevWarnings
-    }, [])
+    )
 
-    if (newWarnings.length === 0) {
-      // eslint-disable-next-line no-param-reassign
-      stylelintResult.errored = false
-    }
-
-    return Object.assign(stylelintResult, { warnings: newWarnings })
+    return Object.assign(stylelintResult, { warnings })
   }
 })
