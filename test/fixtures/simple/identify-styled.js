934c1301e0f657af8dde6c04cb87eeb76f5ff6d4
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-08-26 17:47:25 +0200

    Added failing tests for .attrs fix

---
+++ test/fixtures/simple/identify-styled.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const Button1 = styled.div`
+  bad-selector {
+    color: red;
+  }
+`;
+
+const Button2 = styled(Button1)`
+  bad-selector {
+    color: red;
+  }
+`;
+
+const Image1 = styled.img.attrs({ src: 'url' })`
+  bad-selector {
+    color: red;
+  }
+`;
+
+const Image2 = styled(Image1).attrs({ src: 'newUrl' })`
+  bad-selector {
+    color: red;
+  }
+`;
