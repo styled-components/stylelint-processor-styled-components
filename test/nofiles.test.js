5659806e905a3e4d0ba90b9976f4872b2308af6c
Xianming Zhong <chinesedfan@qq.com>
2017-12-05 23:33:29 +0800

    Handle if no file name given

---
+++ test/nofiles.test.js
@@ -0,0 +1,116 @@
+const stylelint = require('stylelint')
+const path = require('path')
+
+const code = `import styled from 'styled-components';
+const StyledTable = styled(StyledTableBase)\`
+color: red;
+\``
+const processor = path.join(__dirname, '../lib/index.js')
+const rules = {
+  indentation: 2
+}
+
+describe('no files', () => {
+  let codeFilename
+  let data
+
+  // NOTE beforeEach() runs _after_ the beforeAll() hooks of the describe() blocks
+  beforeEach(done => {
+    stylelint
+      .lint({
+        code,
+        codeFilename,
+        syntax: 'scss',
+        config: {
+          processors: [processor],
+          rules
+        }
+      })
+      .then(result => {
+        data = result
+        done()
+      })
+      .catch(err => {
+        // eslint-disable-next-line
+        console.log(err)
+        data = err
+        done()
+      })
+  })
+
+  describe('no codeFilename', () => {
+    beforeAll(() => {})
+
+    it('should have one result', () => {
+      expect(data.results.length).toEqual(1)
+    })
+
+    it('should use the right file', () => {
+      expect(data.results[0].source).toEqual('<input css 2>')
+    })
+
+    it('should have errored', () => {
+      expect(data.errored).toEqual(true)
+    })
+
+    it('should have one warnings', () => {
+      expect(data.results[0].warnings.length).toEqual(1)
+    })
+
+    it('should have the right line number', () => {
+      expect(data.results[0].warnings[0].line).toEqual(3)
+    })
+  })
+
+  describe('not existed codeFilename', () => {
+    beforeAll(() => {
+      codeFilename = 'not-existed-file'
+    })
+
+    it('should have one result', () => {
+      expect(data.results.length).toEqual(1)
+    })
+
+    it('should use the right file', () => {
+      expect(data.results[0].source).toEqual('<input css 7>')
+    })
+
+    it('should have errored', () => {
+      expect(data.errored).toEqual(true)
+    })
+
+    it('should have one warnings', () => {
+      expect(data.results[0].warnings.length).toEqual(1)
+    })
+
+    it('should have the right line number', () => {
+      expect(data.results[0].warnings[0].line).toEqual(3)
+    })
+  })
+
+  describe('existed codeFilename', () => {
+    beforeAll(() => {
+      codeFilename = 'package.json'
+    })
+
+    it('should have one result', () => {
+      expect(data.results.length).toEqual(1)
+    })
+
+    it('should use the right file', () => {
+      expect(data.results[0].source).toEqual(path.resolve(process.cwd(), codeFilename))
+    })
+
+    it('should have errored', () => {
+      expect(data.errored).toEqual(true)
+    })
+
+    it('should have one warnings', () => {
+      expect(data.results[0].warnings.length).toEqual(1)
+    })
+
+    it('should have the right line number', () => {
+      expect(data.results[0].warnings[0].line).toEqual(3)
+    })
+  })
+})
