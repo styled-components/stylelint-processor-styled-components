3f659c07f65d837209426061959449683e7c60b1
Brett Jurgens <brett.jurgens@avant.com>
2018-08-31 13:43:33 -0500

    fix comment

--- test/fixtures/options/invalid-import-name.js
+++ test/fixtures/options/invalid-import-name.js
@@ -1,7 +1,7 @@
 import { something } from 'styled-components'
 
 
-// Empty block, but moduleName isn't set to some-lib
+// Empty block, but importName isn't set to `something`
 // so shouldn't be an error
 const Button = something.div`
 
